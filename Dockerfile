120cc57d4e41166f7f139c6420954eb531af8117
Juraj Selep <jurajselep@gmail.com>
2020-01-29 00:01:21 +0100

    Added code for bootstrap

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM rust:1.39
+COPY . .
+RUN cargo install --path .
+CMD ["tezos-node-bootstrap"]
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+// PoC, needs refactoring
+extern crate reqwest;
+extern crate serde;
+
+#[derive(Debug)]
+pub enum NodeType {
+    Tezedge,
+    Ocaml,
+}
+
+use chrono::DateTime;
+use std::fmt;
+use std::thread;
+use std::thread::JoinHandle;
+use std::time::{Duration, Instant};
+
+impl fmt::Display for NodeType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+fn main() {
+    // NOTE: move to fn if works
+    let measure_tezedge = thread::spawn(move || {
+        let now = Instant::now();
+
+        let bootstrapping_tezedge = create_monitor_node_thread(NodeType::Tezedge);
+        bootstrapping_tezedge.join().unwrap();
+
+        let elapsed = now.elapsed();
+        let sec = (elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64 / 1000_000_000.0);
+        println!("[tezedge] Duration in seconds: {}", sec);
+    });
+
+    let measure_ocaml = thread::spawn(move || {
+        let now = Instant::now();
+
+        let bootstrapping_ocaml = create_monitor_node_thread(NodeType::Ocaml);
+        bootstrapping_ocaml.join().unwrap();
+
+        let elapsed = now.elapsed();
+        let sec = (elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64 / 1000_000_000.0);
+        println!("[ocaml] Duration in seconds: {}", sec);
+    });
+
+    measure_tezedge.join().unwrap();
+    measure_ocaml.join().unwrap();
+}
+
+fn create_monitor_node_thread(node: NodeType) -> JoinHandle<()> {
+    let bootstrap_monitoring_thread = thread::spawn(move || loop {
+        match is_bootstrapped(&node) {
+            Ok(s) => {
+                // empty string means, the rpc server is running, but the bootstraping has not started yet
+                if s != "" {
+                    let desired_timestamp =
+                        // level: 20l cycle:  9
+                        // DateTime::parse_from_rfc3339("2019-10-08T10:17:46Z").unwrap();
+                        // level: 40k cycle: 19
+                        DateTime::parse_from_rfc3339("2019-10-17T10:31:42Z").unwrap();
+                    let block_timestamp = DateTime::parse_from_rfc3339(&s).unwrap();
+
+                    if block_timestamp >= desired_timestamp {
+                        println!("[{}] Done Bootstrapping", node.to_string());
+                        break;
+                    } else {
+                        println!(
+                            "[{}] Bootstrapping . . . timestamp: {}",
+                            node.to_string(),
+                            s
+                        );
+                        thread::sleep(Duration::from_secs(10));
+                    }
+                } else {
+                    println!(
+                        "[{}] Waiting for node to start bootstrapping...",
+                        node.to_string()
+                    );
+                    thread::sleep(Duration::from_secs(10));
+                }
+            }
+            Err(_e) => {
+                // panic!("Error in bootstrap check: {}", e);
+                // NOTE: This should be handled more carefully
+                println!("[{}] Waiting for node to run", node.to_string());
+                println!("[{}] Error: {}", node.to_string(), _e);
+
+                thread::sleep(Duration::from_secs(10));
+            }
+        }
+    });
+    bootstrap_monitoring_thread
+}
+
+#[allow(dead_code)]
+fn is_bootstrapped(node: &NodeType) -> Result<String, reqwest::Error> {
+    
+    let response;
+    match node {
+        NodeType::Tezedge => {
+            response = 
+                reqwest::blocking::get("http://tezedge-node-run:18732/chains/main/blocks/head")?;
+                //reqwest::blocking::get("http://babylon.tezedge.com:38732/chains/main/blocks/head")?;
+        }
+        NodeType::Ocaml => { 
+            response = 
+                reqwest::blocking::get("http://ocaml-node-run:8732/chains/main/blocks/head")?;
+                //reqwest::blocking::get("http://alphanet.simplestaking.com:8732/chains/main/blocks/head")?;
+        }
+    }
+
+    // if there is no response, the node has not started bootstrapping
+    if response.status().is_success() {
+        let response_node: serde_json::value::Value =
+            serde_json::from_str(&response.text()?).expect("JSON was not well-formatted");
+
+        Ok(response_node["header"]["timestamp"]
+            .to_string()
+            .replace("\"", ""))
+    } else {
+        Ok(String::new())
+    }
+}
