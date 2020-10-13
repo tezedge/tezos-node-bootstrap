ecc2d02a0a434ee15d5455f7b44f556e155524b6
Adrian Nagy <adrian.nagy@simplestaking.com>
2020-10-13 17:29:12 +0200

    Update cycle arg

--- src/wrk_test.rs
+++ src/wrk_test.rs
@@ -55,8 +55,8 @@ fn run_wrk(branch: Branch, node: &NodeType, rpc: &str, duration: i32) -> Result<
 
 pub(crate) fn test_rpc_performance(block_header: i32, nodes: Vec<NodeType>, duration: i32) -> Result<(), failure::Error> {
     let rpcs = vec![
-        format!("chains/main/blocks/{}/helpers/baking_rights?all=true&cycle=1", block_header),
-        format!("chains/main/blocks/{}/helpers/endorsing_rights?all&cycle=1", block_header),
+        format!("chains/main/blocks/{}/helpers/baking_rights?all=true&cycle=30", block_header),
+        format!("chains/main/blocks/{}/helpers/endorsing_rights?all&cycle=30", block_header),
         format!("chains/main/blocks/{}/context/constants", block_header),
         format!("chains/main/blocks/{}/votes/listings", block_header),
         // format!("chains/main/blocks/{}/votes/proposals", block_header),
